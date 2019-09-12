748ad11737351d8c7a1a264543f2c9fb9cc1a8c7
Johandaonis02 <Johan.Daun@elev.ga.ntig.se>
2019-09-12 10:51:24 +0200

    First commit

---
+++ Läxor/v37/Läxa.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Läxa {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		String string = input.nextLine();
+		System.out.println(string);
+	}
+}
