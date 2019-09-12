ee65d2db7399221d163ed297c02c386273ec9e84
Johandaonis02 <Johan.Daun@elev.ga.ntig.se>
2019-09-12 10:55:05 +0200

    Hej (namn)

--- Läxor/v37/Läxa.java
+++ Läxor/v37/Läxa.java
@@ -4,8 +4,9 @@ public class Läxa {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("Vad heter du?");
 		Scanner input = new Scanner(System.in);
 		String string = input.nextLine();
-		System.out.println(string);
+		System.out.println("Hej " + string);
 	}
 }
