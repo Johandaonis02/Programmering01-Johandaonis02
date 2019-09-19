2676af9f11673250e5319fcdd7cb354e06e18440
Johandaonis02 <Johan.Daun@elev.ga.ntig.se>
2019-09-19 16:24:59 +0200

    Klart

--- Läxor/v38/Homework.java
+++ Läxor/v38/Homework.java
@@ -8,15 +8,24 @@ public class Homework {
 		
 		Scanner input = new Scanner(System.in);
 		System.out.println("Säg 10 tal");
-		int y = 0;
-		int Big = -2147000000;
-		int Small = 2147000000;
+		int y = 0; //Y håller bara summan av talen ett tag för att hitta genomsnittet.
+		int B = -2147000000; //Sätter den nära -(2^31) så inte startvärdet blir det största värdet.
+		int S = 2147000000; //Sätter den nära 2^31 så inte startvärdet blir det minsta värdet.
 		for(int i = 0; i<10; i++){
 			
 			int x = input.nextInt();
 			y = x + y;
+			
+			if(x > B){
+				B = x;
+			}
+			else if(x < S){
+				S = x;
+			}
 		}
 		
-		System.out.println((float)y/10);
+		System.out.println("Genomsnitt: " + (float)y/10);
+		System.out.println("Störst värdet: " + B);
+		System.out.println("minsta värdet: " + S);
 	}
 }
