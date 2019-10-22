033576f5350e03188b56fe21c6e4bbfc7ff18454
Johandaonis02 <Johan.Daun@elev.ga.ntig.se>
2019-10-22 12:44:29 +0200

    V42 reverse

--- Läxor/V42/Metoder.java
+++ Läxor/V42/Metoder.java
@@ -4,11 +4,21 @@ public class Metoder {
 	public static void main(String[] args) {
 		double radius = 1;
 		System.out.println(volume(radius));
+		String str = "nahoJ";
+		System.out.println(reverse(str));
 		
 		
 	}
 	
 	public static double volume(double radius) {
 		return(Math.PI * Math.pow(radius, 3.) * 4. / 3.);
+	}
+	
+	public static String reverse(String str) {
+		String reverseStr = "";
+		for(int i = str.length() - 1; i >= 0; i--) {
+			reverseStr += str.charAt(i);
+		}
+		return(reverseStr);
 	}
 }
