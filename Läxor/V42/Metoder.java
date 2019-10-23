cafe4903b6e9d717557c2edd1148b3258556509c
Johandaonis02 <Johan.Daun@elev.ga.ntig.se>
2019-10-23 11:48:16 +0200

    V42 Rovarspaket

--- Läxor/V42/Metoder.java
+++ Läxor/V42/Metoder.java
@@ -8,13 +8,13 @@ public class Metoder {
 		System.out.println(reverse(str));
 		char c = 'a';
 		System.out.println(count(str, c));
-		
+		System.out.println(sjorovare(str));
 	}
 	
 	/**
-	 * Calculating volume on a sphere with radius as input
+	 * Sphere's volume
 	 * @param radius
-	 * @return Volume
+	 * @return Calculating volume on a sphere with radius as input
 	 */
 	public static double volume(double radius) {
 		return(Math.PI * Math.pow(radius, 3.) * 4. / 3.);
@@ -23,7 +23,7 @@ public class Metoder {
 	/**
 	 * Reversing strings
 	 * @param str
-	 * @return String backwards
+	 * @return string backwards
 	 */
 	public static String reverse(String str) {
 		String reverseStr = "";
@@ -34,10 +34,10 @@ public class Metoder {
 	}
 	
 	/**
-	 * Calculating how many characters there are in a string
+	 * Calculating how many characters c there are in the string
 	 * @param str
 	 * @param c
-	 * @return How many characters there are in the string.
+	 * @return How many characters c there are in the string.
 	 */
 	public static int count(String str, char c) {
 		int numberOfOccurances = 0;
@@ -47,5 +47,33 @@ public class Metoder {
 			}
 		}
 		return(numberOfOccurances);
+	}
+	
+	/**
+	 * Translate from swedish to rövarspråket
+	 * @param str
+	 * @return The letters in the input string get replaced by two of the same letter with a o in the middle of them if the letters isn't vowel. The letter doesn't get effected if the letter is a vowel
+	 */
+	public static String sjorovare(String str) {
+		char vowels[] = {'e','a','o','i','u','å','y','ä','ö'};
+		boolean testIfCharIsVowel;
+		String rovarspraketWord = "";
+		for(int i = 0; i < str.length(); i++) {
+			testIfCharIsVowel = false;
+			for(int j = 0; j < vowels.length; j++) {
+				if(str.charAt(i) == vowels[j]) {
+					testIfCharIsVowel = true;
+					break;
+				}
+			}
+			if(testIfCharIsVowel == false) {
+				rovarspraketWord += str.charAt(i) + "o" + str.charAt(i);
+			}
+			else {
+				rovarspraketWord += str.charAt(i);
+			}
+			
+		}
+		return(rovarspraketWord);
 	}
 }
