e37a0a751bcf9dbb0062f88d5e8f42875c933519
Johandaonis02 <Johan.Daun@elev.ga.ntig.se>
2019-11-08 09:33:55 +0100

    Update PhysicsLibrary.java
    metod 4

--- Läxor/v45/enums/PhysicsLibrary.java
+++ Läxor/v45/enums/PhysicsLibrary.java
@@ -2,9 +2,10 @@ package enums;
 
 public class PhysicsLibrary {
 	public static void main(String[] args) {
-		
+		double g = 9.81;
 	}
 	
+	//1
 	/**
 	 * Takes a Fahrenheit and calculates the responding Celsius value
 	 * @param Fahrenheit 
@@ -14,7 +15,7 @@ public class PhysicsLibrary {
 		return((fahrenheit - 30) / 1.8);
 	}
 	
-	
+	//2
 	/**
 	 * Takes a Kelvin and calculates the responding Celsius value
 	 * @param Kelvin
@@ -23,4 +24,17 @@ public class PhysicsLibrary {
 	public static double elsiusToKelvin(double kelvin) {
 		return(kelvin + 272.15);
 	}
+	
+	//made question 4 before question 3
+	//4
+	/**
+	 * Calculates the pressure under water with the gravitation constant and depth under water
+	 * @param deep
+	 * @param g
+	 * @return Pa
+	 */
+	public static double pressureUnderWater(double deep, double g) {
+		return(deep * g * 1000);
+	}
+	
 }
